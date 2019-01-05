7e28cdf61aadab36823ec601cf064dca262325c9
Akshay A Padwal <37133590+padwalakshay@users.noreply.github.com>
2019-01-05 17:57:35 +0530

    Update dbmodule.js

--- modules/dbmodule.js
+++ modules/dbmodule.js
@@ -3,8 +3,7 @@ function dbdetails(){
 
 var mongo = require('mongodb').MongoClient;
 var assert = require('assert');
-//var dburl = 'mongodb://localhost:27017/akshaypadwaldb'
-var dburl = 'mongodb://akshay:admin@ds231715.mlab.com:31715/akshaypadwaldb';
+var dburl = 'mongodb://akshay:your@exampletestdbname';
 return {"mongo":mongo,"url":dburl,"assert":assert};
 }
 module.exports = dbdetails;
